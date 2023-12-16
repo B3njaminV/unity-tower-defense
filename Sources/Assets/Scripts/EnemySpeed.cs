6b09b2b60478de606a60a693d485720e29f65c6b
Benjamin <Benjamin.valleix@gmail.com>
2023-12-16 22:04:55 +0100

    :zap: Ajout stratégie sur la vitesse de déplacement des gameobject + début gamemanager

---
+++ Sources/Assets/Scripts/EnemySpeed.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using UnityEngine;
+
+namespace Assets.Scripts
+{
+    internal class EnemySpeed : IEnemyMovement
+    {
+        Vector3 IEnemyMovement.deplacement()
+        {
+            return new Vector3(0, 0, 0.1f);
+        }
+    }
+}
--- Sources/Assets/Scripts/GameManager.cs
+++ Sources/Assets/Scripts/GameManager.cs
@@ -10,13 +10,15 @@ public class GameManager : MonoBehaviour
 
     public int score = 0;
 
+    private List<EnemyModel> _enemies;
+
     public static GameManager Instance
     {
         get
         {
             if (_instance == null)
             {
-                _instance = GameObject.FindObjectOfType<GameManager>();
+                _instance = new GameManager();
                 DontDestroyOnLoad(_instance.gameObject);
                 // On détruit pas l'objet GameManager quand on change de scène
             }
@@ -32,7 +34,7 @@ public class GameManager : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        
+        _enemies = new List<EnemyModel>();       
     }
 
     // Update is called once per frame
@@ -49,5 +51,17 @@ public class GameManager : MonoBehaviour
     internal void Reculer()
     {
         Debug.Log("Reculer");
+    }
+
+    public void AddEnemy(EnemyModel enemy)
+    {
+        _enemies.Add(enemy);
+    }
+
+    public void SpawnEnemy()
+    {
+        GameObject enemy = GameObject.CreatePrimitive(PrimitiveType.Cube);
+        enemy.AddComponent<EnemyModel>();
+        AddEnemy(enemy.GetComponent<EnemyModel>());
     }
 }
