4b3d86b3d0ae16ad3bd2c0168e5fe9b3e694646a
Augustin <giraudieraugustin@gmail.com>
2024-01-14 15:40:56 +0100

    Scripstable objects for scenarios and manager for spawning zombies : ok

---
+++ Sources/Assets/Scripts/Enums/GameLineEnum.cs
@@ -0,0 +1,8 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public enum GameLineEnum
+{
+    Col1, Col2, Col3, Col4, Col5
+}
---
+++ Sources/Assets/Scripts/ScenarioManager.cs
@@ -0,0 +1,44 @@
+using System.Collections;
+using UnityEngine;
+
+public class ScenarioManager : MonoBehaviour
+{
+
+    [SerializeField]
+    private SO_Scenario scenario;
+
+    private float startingTime;
+    private GameObject[] spawners;
+
+    private int nextSpawn = 0;
+
+    public float ElapsedTime { get { return Time.time - startingTime; } }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        startingTime = Time.time;
+        spawners = GameObject.FindGameObjectsWithTag("ZombieSpawner");
+        // shuffle spawners
+        ListShuffler.Shuffle(spawners);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(scenario == null || nextSpawn >= scenario.spawns.Count) { return; }
+
+        SO_Scenario.ZombieSpawn currentSpawn = scenario.spawns[nextSpawn];
+
+        if (ElapsedTime >= currentSpawn.spawnTime )
+        {
+            // Spawn Zombie
+            if((int)currentSpawn.line < spawners.Length && currentSpawn.ZombieType < scenario.zombies.Count)
+            {
+                Instantiate(scenario.zombies[currentSpawn.ZombieType], spawners[(int)currentSpawn.line].GetComponent<Transform>());
+            }
+            nextSpawn++;
+        }
+    }
+
+}
