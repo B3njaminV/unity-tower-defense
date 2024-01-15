a91b23ffbe605362c47fe49c327bf06d90378135
Benjamin <Benjamin.valleix@gmail.com>
2024-01-15 16:22:14 +0100

    ✨ Ajout GameOver scene

--- Sources/Assets/Scripts/GameOverCollisionScript.cs
+++ Sources/Assets/Scripts/GameOverCollisionScript.cs
@@ -7,14 +7,10 @@ public class GameOverCollisionScript : MonoBehaviour
 {
     private void OnTriggerEnter2D(Collider2D collision)
     {
-        Debug.Log("OnTriggerEnter2D");
         if (collision.gameObject.tag == "Ennemy")
         {
-            Debug.Log("Game Over");
+            GameObject.Find("GameOver").GetComponent<Canvas>().enabled = true;
+            Time.timeScale = 0;
         }
-
-        GameObject.Find("GameOver").GetComponent<Canvas>().enabled = true;
-
-        Time.timeScale = 0;
     }
 }
