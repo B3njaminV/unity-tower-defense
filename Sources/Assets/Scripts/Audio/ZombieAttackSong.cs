1db5e5edc1d23dd4e5d40294ab126176bea9cd05
Benjamin <Benjamin.valleix@gmail.com>
2024-01-20 17:49:55 +0100

    🔨Add soundtrack (Resources/)

---
+++ Sources/Assets/Scripts/Audio/ZombieAttackSong.cs
@@ -0,0 +1,34 @@
+﻿using UnityEngine;
+
+public class ZombieAttackSong : IAudioState
+{
+    private AudioManager _audioManager;
+
+    public ZombieAttackSong(AudioManager audioManager)
+    {
+        _audioManager = audioManager;
+    }
+
+    public void OnEnterState()
+    {
+        _audioManager.GetAudioSource().loop = false;
+    }
+
+    public void PlaySong()
+    {
+        Debug.Log("Lancement musique : zattack_soundtrack");
+        AudioClip songClip = Resources.Load<AudioClip>("zattack_soundtrack");
+
+        if (songClip != null)
+        {
+            _audioManager.GetAudioSource().clip = songClip;
+            _audioManager.GetAudioSource().Play();
+        }
+        else
+        {
+            Debug.LogError("Musique non trouvé : zattack_soundtrack");
+        }
+    }
+}
+
+
